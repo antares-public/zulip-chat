8c45b8901743e2735853e0c95c7c767aec76aca0
antaresofficial <antaresofficial@icloud.com>
2021-11-17 16:52:48 +0500

    bugfix

--- src/pages/Stream/Stream.tsx
+++ src/pages/Stream/Stream.tsx
@@ -1,18 +1,48 @@
-import React from "react";
-import { IMessageZulip } from "../../interfaces";
+import React, { useCallback, useEffect, useState } from "react";
+import { IMessageZulip, IStream } from "../../interfaces";
 import { config } from "../../constants/config";
 import { Button, Form, Grid, Comment } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 import { MessageItem } from "../../components/MessageItem";
+import zulipInit from "zulip-js";
+import { Loading } from "../../components/Loading";
 
-export const Stream: React.FC<any> = ({
-  handleSend,
-  stream,
-  messages,
-  message,
-  setMessage,
-}) => {
-  if (!stream) <h1>Not found</h1>;
+export const Stream: React.FC<any> = ({ stream, id }) => {
+  const [message, setMessage] = useState<IMessageZulip["content"]>("");
+  const [messages, setMessages] = useState<IStream>([]);
+
+  const hadlerFetchMessages = useCallback(async () => {
+    const client = await zulipInit(config);
+
+    const readParams = {
+      anchor: "newest",
+      num_before: 5,
+      num_after: 0,
+      narrow: [{ operator: "stream", operand: stream.name }],
+    };
+    const { messages } = await client.messages.retrieve(readParams);
+    setMessages(messages);
+  }, [stream.name]);
+
+  const handleSend = async () => {
+    const client = await zulipInit(config);
+    const params = {
+      to: stream.name,
+      type: "stream",
+      topic: "Castle",
+      content: message,
+    };
+    await client.messages.send(params);
+    setMessage("");
+  };
+
+  useEffect(() => {
+    hadlerFetchMessages();
+  }, [hadlerFetchMessages]);
+
+  if (!messages.length) {
+    return <Loading />;
+  }
 
   return (
     <Grid.Column style={{ maxWidth: 600, margin: "20px 0" }}>
--- src/pages/Stream/StreamContainer.tsx
+++ src/pages/Stream/StreamContainer.tsx
@@ -1,46 +1,24 @@
 import React, { useCallback, useEffect, useState } from "react";
 import { useParams } from "react-router";
-import { IMessageZulip, IStream } from "../../interfaces";
+import { IStream } from "../../interfaces";
 import zulipInit from "zulip-js";
 import { config } from "../../constants/config";
-import { Stream } from "./Stream";
 import { Loading } from "../../components/Loading";
+import { Stream } from "./Stream";
 
 export const StreamContainer = () => {
   const [stream, setStream] = useState<IStream>();
-  const [message, setMessage] = useState<IMessageZulip["content"]>("");
-  const [messages, setMessages] = useState<IStream>();
   const { id } = useParams<{ id: string }>();
 
-  const hadlerFetchMessages = useCallback(async () => {
+  const handleGetStream = useCallback(async () => {
     const client = await zulipInit(config);
-    const readParams = {
-      anchor: "newest",
-      num_before: 5,
-      num_after: 0,
-      narrow: [{ operator: "stream", operand: "core team" }],
-    };
     const { streams } = await client.streams.retrieve();
-    const { messages } = await client.messages.retrieve(readParams);
-    setMessages(messages);
-    setStream(streams.find((d: IStream) => Number(id) === d.stream_id) || null);
+    setStream(streams.find((d: IStream) => Number(id) === d.stream_id));
   }, [id]);
 
-  const handleSend = useCallback(async () => {
-    const client = await zulipInit(config);
-    const params = {
-      to: stream.name,
-      type: "stream",
-      topic: "Castle",
-      content: message,
-    };
-    await client.messages.send(params);
-    setMessage("");
-  }, [message, stream?.name]);
-
   useEffect(() => {
-    hadlerFetchMessages();
-  }, [hadlerFetchMessages, handleSend]);
+    handleGetStream();
+  }, [handleGetStream]);
 
   if (!stream) {
     return <Loading />;
@@ -49,10 +27,11 @@ export const StreamContainer = () => {
   return (
     <Stream
       stream={stream}
-      handleSend={handleSend}
-      setMessage={setMessage}
-      messages={messages}
-      message={message}
+      id={id}
+      // handleSend={handleSend}
+      // setMessage={setMessage}
+      // messages={messages}
+      // message={message}
     />
   );
 };
